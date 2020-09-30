2c7a2ac470dcbf5e97c3720482df37d071e8a446
jinwoo <jintakk6531@gmail.com>
2020-09-30 17:01:16 +0900

    9/30

--- client/src/components/views/NavBar/Sections/RightMenu.js
+++ client/src/components/views/NavBar/Sections/RightMenu.js
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import { Menu } from "antd";
 import axios from "axios";
@@ -32,10 +31,7 @@ function RightMenu(props) {
     );
   } else {
     return (
-      <Menu mode={props.mode}>
-        <Menu.Item key="upload">
-          <a href="/video/upload">Video</a>
-        </Menu.Item>
+      <Menu mode={props.mode}>        
         <Menu.Item key="logout">
           <a onClick={logoutHandler}>Logout</a>
         </Menu.Item>
