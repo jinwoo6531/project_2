a9f66693e550d568031e6ad0e8c3ae18f9d9b60c
jinwoo <jintakk6531@gmail.com>
2020-10-24 17:31:56 +0900

    redux test

--- client/src/index.js
+++ client/src/index.js
@@ -15,6 +15,7 @@ import { createStore, applyMiddleware } from 'redux';
 import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk';
 
+//promise가 store에서 해석하게 해줄수있게 
 const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 ReactDOM.render(
@@ -30,7 +31,5 @@ ReactDOM.render(
         </BrowserRouter>
     </Provider>
     , document.getElementById('root'));
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
